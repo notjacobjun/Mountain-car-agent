889c1d02ac397896782dd8849a609c191f130237
Jacob Jun <jacobsunjun@gmail.com>
2022-06-16 10:43:35 -0400

    Add metric tracking features

--- q_learning_1.py
+++ q_learning_1.py
@@ -1,6 +1,8 @@
+from cProfile import label
 from xml.dom.pulldom import END_ELEMENT
 import numpy as np
 import gym
+import matplotlib.pyplot as plt
 
 env = gym.make("MountainCar-v0")
 
@@ -20,13 +22,13 @@ LEARNING_RATE = 0.1
 # this is the time-decay to ensure that more recent observations are weighted more than less recent observations
 DISCOUNT = 0.95
 # this is for epslion greedy learning to help explore more
-EPISODES = 25000
+EPISODES = 2000
 epsilon = 0.5
 START_EPSILON_DECAYING = 1
 END_EPSILON_DECAYING = EPISODES // 2
 epsilon_decay_value = epsilon/(END_EPSILON_DECAYING - START_EPSILON_DECAYING)
 
-SHOW_EVERY = 2000
+SHOW_EVERY = 500
 
 DISCRETE_OS_SIZE = [20] * len(env.observation_space.high)
 # Change continuous observation space into discrete space. This is to help save time in Q-learning
@@ -37,7 +39,11 @@ discrete_os_win_size = (env.observation_space.high -
 q_table = np.random.uniform(
     low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
 
+ep_rewards = []
+aggr_ep_rewards = {"ep": [], "avg": [], "min": [], "max": []}
+
 for episode in range(EPISODES):
+    episode_reward = 0
     if episode % SHOW_EVERY == 0:
         print(episode)
         render = True
@@ -55,6 +61,7 @@ for episode in range(EPISODES):
         else:
             action = np.random.randint(0, env.action_space.n)
         new_state, reward, done, _ = env.step(action)
+        episode_reward += reward
 
         new_discrete_state = get_discrete_state(new_state)
         if render:
@@ -79,5 +86,27 @@ for episode in range(EPISODES):
         discrete_state = new_discrete_state
     if END_EPSILON_DECAYING >= episode >= START_EPSILON_DECAYING:
         epsilon -= epsilon_decay_value
+    # add the curren episode reward to the list of episode rewards
+    ep_rewards.append(episode_reward)
+
+    # Note that this is the industry standard way to write "if episode % SHOW_EVERY == 0"
+    if not episode % SHOW_EVERY:
+        # this is taking the mean of the last SHOW_EVERY episode rewards
+        average_reward = sum(
+            ep_rewards[-SHOW_EVERY:]) / len(ep_rewards[-SHOW_EVERY:])
+        aggr_ep_rewards['ep'].append(episode)
+        aggr_ep_rewards['avg'].append(average_reward)
+        aggr_ep_rewards['min'].append(min(ep_rewards[-SHOW_EVERY:]))
+        aggr_ep_rewards['max'].append(max(ep_rewards[-SHOW_EVERY:]))
+
+        print(
+            f"Episode: {episode} avg: {average_reward} min: {min(ep_rewards[-SHOW_EVERY:])} max: {max(ep_rewards[-SHOW_EVERY:])}")
+
 
 env.close()
+
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards["avg"], label="avg")
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards["min"], label="min")
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards["max"], label="max")
+plt.legend(loc=4)
+plt.show()
